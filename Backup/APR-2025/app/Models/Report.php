4e5d3b4f0eddbb23f4d10395f8583e8e5f8ddc33
Cp. Cool <rahulsoni0000001@gmail.com>
2025-04-02 18:15:56 +0530

    Apr 1

---
+++ Backup/APR-2025/app/Models/Report.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbl_reports';
+    protected $fillable = [
+        'worker_name',
+        'sr_no_fiber',
+        'm_j',
+        'type',
+        'section',
+        'note1',
+        'note2',
+        'remark',
+        'status',
+        'part',
+        'temp',
+        'r_status',
+        'f_status',
+        'party_name',
+        'sale_status',
+        'stock_status',
+        'final_amount',
+    ];
+
+    public function tbl_leds()
+    {
+        return $this->hasMany(TblLed::class, 'report_id', 'id');
+    }
+
+    public function tbl_cards()
+    {
+        return $this->hasMany(TblCard::class, 'report_id', 'id');
+    }
+    public function tbl_type() // Singular, since it's a belongsTo relationship
+    {
+        return $this->belongsTo(Tbltype::class, 'type', 'id');
+    }
+
+}
